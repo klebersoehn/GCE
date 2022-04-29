0089722e1b80e85f815df334fc54230823fe662e
Kleber Francisco Soehn da Costa <kleberfscosta@gmail.com>
2022-04-29 12:54:34 -0400

    Projeto Criado

--- GCE.Domain/Models/Enum.cs
+++ GCE.Domain/Models/Enum.cs
@@ -28,4 +28,43 @@ namespace GCE.Domain.Models
         [Display(Name = "Jurídica")]
         Juridica = 2
     }
+
+    public enum TipoEmpresa
+    {
+        [Display(Name = "Ltda")]
+        Ltda = 1,
+
+        [Display(Name = "Me")]
+        Me = 2
+    }
+
+    public enum Porte
+    {
+        [Display(Name = "Outros")]
+        Outros = 1
+    }
+
+    public enum CaracterizacaoCaptal
+    {
+        [Display(Name = "Outros")]
+        Outros = 1
+    }
+
+    public enum EstadoCivil
+    {
+        [Display(Name = "Solteiro")]
+        Solteiro = 1,
+
+        [Display(Name = "Casado")]
+        Casado = 2
+    }
+
+    public enum Genero
+    {
+        [Display(Name = "Feminino")]
+        Feminino = 1,
+
+        [Display(Name = "Masculino")]
+        Masculino = 2
+    }
 }
--- GCE.Domain/Models/Pessoa.cs
+++ GCE.Domain/Models/Pessoa.cs
@@ -8,14 +8,15 @@ using System.Threading.Tasks;
 
 namespace Gce.Domain.Models
 {
-    public partial class Pessoa : BaseEntity
+    public partial class Pessoa : BaseEntity, IValidatableObject
     {
         [Key]
         public int Id { get; set; }
 
+        //Quando CNPJ é Razão Social
         [Required]
         [StringLength(120)]
-        //[Display(Name = "Nome", ResourceType = typeof(Resources.Models.Pessoa))]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
 
         [StringLength(120)]
@@ -32,46 +33,76 @@ namespace Gce.Domain.Models
         [Display(Name = "Data Nascimento")]
         public DateTime? DataNascimento { get; set; }
 
+        [DataType(DataType.Date)]
+        [Display(Name = "Data da Constituição")]
+        public DateTime? DataConstituicao { get; set; }
+
+        [Required]
         [StringLength(20)]
         [DataType(DataType.PhoneNumber)]
-        //[Display(Name = "Fone1", ResourceType = typeof(Resources.Models.Pessoa))]
+        [Display(Name = "Fone 1")]
         public string Fone1 { get; set; }
 
         [StringLength(20)]
         [DataType(DataType.PhoneNumber)]
-        //[Display(Name = "Fone2", ResourceType = typeof(Resources.Models.Pessoa))]
+        [Display(Name = "Fone 2")]
         public string Fone2 { get; set; }
 
         [StringLength(20)]
         [DataType(DataType.PhoneNumber)]
-        //[Display(Name = "Fone2", ResourceType = typeof(Resources.Models.Pessoa))]
+        [Display(Name = "Fone 3")]
         public string Fone3 { get; set; }
 
         [Required]
         [Display(Name = "E-mail")]
         [EmailAddress]
-        [Index(IsUnique = true)]
         [StringLength(100)]
         public string Email { get; set; }
 
+        [Display(Name = "Website")]
+        [StringLength(200)]
+        public string WebSite { get; set; }
+
+        [Display(Name = "Profissão")]
+        [StringLength(200)]
+        public string Profissao { get; set; }
+
+        [Display(Name = "Nacionalidade")]
+        [StringLength(100)]
+        public string Nacionalidade { get; set; }
+
+        [Display(Name = "Quantidade de Quota")]
         public int? QuantidadeQuota { get; set; }
 
         [DataType(DataType.Currency)]
         public decimal? ValorQuota { get; set; }
 
-        [Required]
+        [Display(Name = "Capital Social")]
         [DataType(DataType.Currency)]
-        public decimal CaptalInicial { get; set; }
+        public decimal CaptalSocial { get; set; }
 
-        [Required]
-        [StringLength(1)]
-        //[Display(Name = "Tipo", ResourceType = typeof(Resources.Models.Pessoa))]
+        [Display(Name = "Tipo de Pessoa")]
         public TipoPessoa TipoPessoa { get; set; }
+
+        [Display(Name = "Tipo de Empresa")]
+        public TipoEmpresa TipoEmpresa { get; set; }
+
+        public Porte Porte { get; set; }
 
         [Required]
         public Nacional Nacional { get; set; }
 
+        [Display(Name = "Gênero")]
+        public Genero Genero { get; set; }
+
+        [Display(Name = "Situação")]
         public Situacao Situacao { get; set; }
+
+        [Display(Name = "Estado Civil")]
+        public EstadoCivil EstadoCivil { get; set; }
+
+        [Display(Name = "Caracterização do Capital")]
+        public CaracterizacaoCaptal CaracterizacaoCaptal { get; set; }
 
 
         public Pessoa()
@@ -79,5 +110,53 @@ namespace Gce.Domain.Models
 
         }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (TipoPessoa == TipoPessoa.Juridica)
+            {
+                if (string.IsNullOrEmpty(Cnpj))
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "Cnpj"), new[] { nameof(Cnpj) });
+                }
+
+                if (TipoEmpresa == 0)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "TipoEmpresa"), new[] { nameof(TipoEmpresa) });
+                }
+
+                if (Porte == 0)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "Porte"), new[] { nameof(Porte) });
+                }
+
+                if (CaptalSocial < 0)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "CaptalSocial"), new[] { nameof(CaptalSocial) });
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(Cpf))
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "Cpf"), new[] { nameof(Cpf) });
+                }
+                if (string.IsNullOrEmpty(Profissao))
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "Profissao"), new[] { nameof(Profissao) });
+                }
+                if (TipoEmpresa == 0)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "TipoEmpresa"), new[] { nameof(TipoEmpresa) });
+                }
+                if (!DataNascimento.HasValue)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "DataNascimento"), new[] { nameof(DataNascimento) });
+                }
+                if (Genero == 0)
+                {
+                    yield return new ValidationResult(string.Format("O campo {0} é obrigatório.", "Genero"), new[] { nameof(Genero) });
+                }
+            }
+        }
     }
 }
